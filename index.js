0b7bd69a7e0950bcc234ab4348de2cca7cbb536e
Vishnu-IOT <93981819+Vishnu-IOT@users.noreply.github.com>
2025-12-09 11:41:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express'); // Importing express
 const app = express(); // Creating an express app
 const startpoint = require("./routes/login.js")
-// const cors = require("cors");
+const cors = require("cors");
 require("dotenv").config();
 
 
-// app.use(cors("http://localhost:3000"));
+app.use(cors("auth-frontend-lime-zeta.vercel.app"));
 app.use(express.json());
 app.use("/admin",startpoint);
 console.log("passed index")
@@ -17,4 +17,5 @@ console.log("passed index")
 const port = process.env.SERVER_PORT;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+
 });
