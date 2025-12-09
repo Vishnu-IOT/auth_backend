86ac694ec1d46fb61d64d54a5b9042bb0a147038
Vishnu-IOT <93981819+Vishnu-IOT@users.noreply.github.com>
2025-12-09 12:28:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const cors = require("cors");
 require("dotenv").config();
 
 
-// app.use(cors("auth-frontend-lime-zeta.vercel.app"));
-app.use(cors());
+app.use(cors("https://authbackend-production-b892.up.railway.app"));
+// app.use(cors());
 app.use(express.json());
 app.use("/admin",startpoint);
 console.log("passed index")
@@ -20,4 +20,5 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 
 });
+
 
