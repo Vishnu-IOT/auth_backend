0e5aa247555243dcd99ced621210627ae57b8629
Vishnu-IOT <93981819+Vishnu-IOT@users.noreply.github.com>
2025-12-31 11:03:25 +0530

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -11,10 +11,12 @@ router.get('/values', verifycred, getValues);
 router.post('/fileup', errorhandle);
 router.get("/verify", verifycred);
 router.post("/verifyotp", verifycred, otpverify);
-router.post("/emailverify", sendemail)
-router.post("/savepass", updatepass)
+router.post("/emailverify", sendemail);
+router.post("/savepass", updatepass);
+router.post("/madeit",tableCreation);
 
 
 console.log("passed login");
+
 
 module.exports = router;
